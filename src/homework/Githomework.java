7f328c8a69b3d69a58fb7c71ee9abb1351c319bb
rogef <rogef@Roge-Ram.attlocal.net>
2024-09-10 20:43:20 -0400

    Rest

--- src/homework/Githomework.java
+++ src/homework/Githomework.java
@@ -1,13 +1,20 @@
 package homework;
 
 public class Githomework {
-	
-	public static void main(String [] args) {
+
+	public static void main(String[] args) {
 		System.out.println("BATCH 12: Class Lecture");
 	}
-	
-	public static void add(int a, int b) {
-		System.out.println(a+b);
 
-}
+	public static void add(int a, int b) {
+		System.out.println(a + b);
+	}
+
+	public static void rest(int a, int b) {
+		System.out.println(a - b);
+	}
+
+	
+	
+
 }
