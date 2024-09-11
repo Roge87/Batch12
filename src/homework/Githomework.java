a6ff0b54c94f8f7b62dded08e59d2128b39f5fed
rogef <rogef@Roge-Ram.attlocal.net>
2024-09-10 21:15:24 -0400

    Push

--- src/homework/Githomework.java
+++ src/homework/Githomework.java
@@ -1,39 +1,6 @@
 package homework;
 
 public class Githomework {
-
-	public static void main(String[] args) {
-		System.out.println("BATCH 12: Class Lecture");
-		int age = 11;
-		String name = "Jhon";
-		// This is coming from github
-	}
-
-	public static void add(int a, int b) {
-		System.out.println(a + b);
-		int cant = 34;
-		System.out.println(cant);
-		//Test add
-	}
-
-	public static void rest(int a, int b) {
-		System.out.println(a - b);
-		int rest = 45;
-		System.out.println(rest);
-		//Test rest
-	}
-
-	public static void div(int a, int b) {
-		System.out.println(a / b);
-		System.out.println("is equal to 8");
-		//test div
-	}
-
-	public static void mult(int a, int b) {
-		System.out.println(a * b);
-		System.out.println("equal 89");
-		// Test mult
-	}
 	
 
 }
