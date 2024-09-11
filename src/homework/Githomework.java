2c486a41207055510b56e3d291ae49015bb23055
rogef <rogef@Roge-Ram.attlocal.net>
2024-09-10 20:37:04 -0400

    added new method

--- src/homework/Githomework.java
+++ src/homework/Githomework.java
@@ -5,5 +5,9 @@ public class Githomework {
 	public static void main(String [] args) {
 		System.out.println("BATCH 12: Class Lecture");
 	}
+	
+	public static void add(int a, int b) {
+		System.out.println(a+b);
 
+}
 }
