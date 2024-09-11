198b702bfab775c523f6f5036e242c392caa321f
rogef <rogef@Roge-Ram.attlocal.net>
2024-09-10 22:07:31 -0400

    test

--- src/homework/Githomework.java
+++ src/homework/Githomework.java
@@ -2,16 +2,5 @@ package homework;
 
 public class Githomework {
 	
-	public void display() {
-		System.out.println("Roger");
-	}
-/**
-	 Thisi from Github
-	 Java is fun
-	Git is easy
-	API is not fun
-*/
-	String firstName = "Jhon";
-	String lastName = "Cena";
-	String city = "Reston";
+	
 }
